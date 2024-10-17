dfd6fc6fcd8f86dd2dff7bdc2290da89ca2c2783
Daniel Stefanov <97540472+ludsmotika@users.noreply.github.com>
2024-10-17 17:10:10 +0300

    first python homework

---
+++ first_homework/first_homework.py
@@ -0,0 +1,14 @@
+viktors_ingredients = [
+    'чушки', 'домати', 'моркови', 'ябълки',
+    'сол', 'черен пипер', 'кимион', 'зехтин'
+]
+georgis_ingredients = (
+    'чушки', 'домати', 'патладжан',
+    'люти чушки', 'олио', 'захар',
+    'чубрица', 'черен пипер', 'врачанска ракия'
+)
+shopping_list = (viktors_ingredients + list(georgis_ingredients))[::-1]
+unique_ingredients = set(shopping_list)
+ingredient_quantities = dict.fromkeys(unique_ingredients, 5)
+ingredient_quantities['skyr'] = 1
+number_of_ingredients_to_buy = len(ingredient_quantities)
